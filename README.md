287d5e049c21a3a53b0ee6ab8879700644683b70
thinkpad-l13-yoga <pulipuli.chen@gmail.com>
2025-02-15 05:53:56 +0800

    西元2025年02月15日 (週六) 05時53分56秒 CST

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # test-bisheng-20250215-0436
+
+port 3001
+
+什麼是RAG?
+What is RAG?
---
+++ ragflow/web/src/components/knowledge-base-item.tsx
@@ -0,0 +1,48 @@
+import { DocumentParserType } from '@/constants/knowledge';
+import { useTranslate } from '@/hooks/common-hooks';
+import { useFetchKnowledgeList } from '@/hooks/knowledge-hooks';
+import { UserOutlined } from '@ant-design/icons';
+import { Avatar, Form, Select, Space } from 'antd';
+
+const KnowledgeBaseItem = () => {
+  const { t } = useTranslate('chat');
+
+  const { list: knowledgeList } = useFetchKnowledgeList(true);
+
+  const filteredKnowledgeList = knowledgeList.filter(
+    (x) => x.parser_id !== DocumentParserType.Tag,
+  );
+
+  const knowledgeOptions = filteredKnowledgeList.map((x) => ({
+    label: (
+      <Space>
+        <Avatar size={20} icon={<UserOutlined />} src={x.avatar} />
+        {x.name}
+      </Space>
+    ),
+    value: x.id,
+  }));
+
+  return (
+    <Form.Item
+      label={t('knowledgeBases')}
+      name="kb_ids"
+      tooltip={t('knowledgeBasesTip')}
+      rules={[
+        {
+          required: true,
+          message: t('knowledgeBasesMessage'),
+          type: 'array',
+        },
+      ]}
+    >
+      <Select
+        mode="multiple"
+        options={knowledgeOptions}
+        placeholder={t('knowledgeBasesMessage')}
+      ></Select>
+    </Form.Item>
+  );
+};
+
+export default KnowledgeBaseItem;
